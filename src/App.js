cd18e05e8542595b96ab1cae5b3a65752645a3d1
Dilan Sarith Hewawitharana <42795493+dilanHewawitharana@users.noreply.github.com>
2021-07-17 12:51:29 +0900

    Finish weekend project

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+import { useEffect, useState } from "react";
+import './styles.css'
+import ContactCard from "./ContactCard";
+
+
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=10')
+      .then((response) => response.json())
+      .then((data) => {
+        setContacts(data.results);
+      });
+  }, []);
+
+  return (
+    <>
+      {contacts.map((contact) => (
+        <ContactCard 
+          avatar={contact.picture.large}
+          name={contact.name.first + ' ' + contact.name.last} 
+          email={contact.email} 
+          age={contact.dob.age}/>
+      ))}
+    </>
+
+
+  );
+
+};
+
+export default App;
